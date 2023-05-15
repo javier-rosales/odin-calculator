ab43a95ce044584e7e6a23e0352991da0bc7b2b6
Javier Rosales <cyjav.dev@gmail.com>
2023-05-15 14:43:32 -0600

    Create references to HTML elements

---
+++ assets/scripts/script.js
@@ -0,0 +1,15 @@
+// References to HTML elements
+
+const inputScreen = document.querySelector(".screen__input")
+const resultScreen = document.querySelector(".screen__result")
+
+const buttonAllClear = document.querySelector(".button.all-clear")
+const buttonBackspace = document.querySelector(".button.backspace")
+
+const buttonsNumbers = document.querySelectorAll(".button.number")
+
+const buttonDecimalPoint = document.querySelector(".button.decimal-point")
+
+const buttonsOperators = document.querySelectorAll(".button.operator")
+
+const buttonResult = document.querySelector(".button.result")

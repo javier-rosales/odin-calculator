a5afa27090b6cdf119474732fa2336e2a04e5400
Javier Rosales <cyjav.dev@gmail.com>
2023-05-15 22:02:45 -0600

    Add event listeners to calculator buttons

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -11,3 +11,26 @@ const buttonDecimalPoint = document.querySelector(".button.decimal-point")
 const buttonsNumbers = document.querySelectorAll(".button.number")
 const buttonsOperators = document.querySelectorAll(".button.operator")
 const buttonResult = document.querySelector(".button.result")
+
+// Add event listeners to calculator buttons
+
+buttonAllClear.addEventListener("click", () => updateInput("all-clear"))
+buttonBackspace.addEventListener("click", () => updateInput("backspace"))
+
+buttonDecimalPoint.addEventListener("click", () => updateInput("add-character", "."))
+
+buttonsNumbers.forEach(buttonNumber => {
+    buttonNumber.addEventListener("click", event => {
+        number = event.target.dataset.number
+        updateInput("add-character", number)
+    })
+})
+
+buttonsOperators.forEach(buttonOperator => {
+    buttonOperator.addEventListener("click", event => {
+        operator = event.target.dataset.operator
+        updateInput("add-character", operator)
+    })
+})
+
+buttonResult.addEventListener("click", () => updateResult())

9d1217765dd063e261001da9fa6ff768e5addf58
Javier Rosales <cyjav.dev@gmail.com>
2023-05-15 21:56:48 -0600

    Add global variables
    - "input" and "result" contain current values of the calculator input
    expression and its result respectively
    - Some HTML element references were reordered

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,15 +1,13 @@
-// References to HTML elements
+// Global variables
+
+let input = ""
+let result = ""
 
 const inputScreen = document.querySelector(".screen__input")
 const resultScreen = document.querySelector(".screen__result")
-
 const buttonAllClear = document.querySelector(".button.all-clear")
 const buttonBackspace = document.querySelector(".button.backspace")
-
-const buttonsNumbers = document.querySelectorAll(".button.number")
-
 const buttonDecimalPoint = document.querySelector(".button.decimal-point")
-
+const buttonsNumbers = document.querySelectorAll(".button.number")
 const buttonsOperators = document.querySelectorAll(".button.operator")
-
 const buttonResult = document.querySelector(".button.result")
